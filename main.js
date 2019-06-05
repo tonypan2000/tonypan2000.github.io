0298b52e50fa2ae4a14cbb7bcfede6b3e697a554
tonypan <tonypan@umich.edu>
2019-06-05 16:07:40 +0800

    added click to scroll

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
   })
+
+  $(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
