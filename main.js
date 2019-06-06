e176698c6d579d310e3a9ad6474d008690cad24f
tonypan <tonypan@umich.edu>
2019-06-06 14:24:23 +0800

    scroll, programming, reduce image size

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
   $(function() {
     $('a[href*=#]').on('click', function(e) {
       e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 50, 'linear');
     });
   });
+
+  $('.carousel').carousel({
+    interval: 2000
+  })
+  
+  (function($) {
+    "use strict";
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+  })(jQuery);
